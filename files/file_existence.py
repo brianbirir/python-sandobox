7ba3d6b612f32238fbb53478fddad897eb726412
Brian Birir <brianbirir@gmail.com>
2018-09-05 10:02:31 +0300

    Check if file exists methods

---
+++ files/file_existence.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path 
+
+# Brute force with a try-except block
+try: 
+    fh = open('/path/to/file', 'r') 
+except FileNotFoundError: 
+    pass
+
+# Leverage the OS package
+import os 
+exists = os.path.isfile('/path/to/file')
+
+# Wrap the path in an object for enhanced functionality
+from pathlib import Path
+config = Path('/path/to/file') 
+if config.is_file(): 
+    pass
