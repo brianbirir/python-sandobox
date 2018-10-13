1e6f5b5689b11e95edfd9f44263d516bc63c5304
Brian Birir <brianbirir@gmail.com>
2018-10-13 12:19:21 +0300

    Functions as variables

---
+++ python_tricks/functions_as_arguments_variables.py
@@ -0,0 +1,16 @@
+'''
+functions as:
+
+1. Arguments for other functions
+2. Values returned from other functions
+3. assigned variables that can be stored in data structures
+
+'''
+def myfunc(a, b):
+	return a+b
+
+# list data structure
+funcs = [myfunc]
+funcs[0]
+
+funcs[0](2, 3)
